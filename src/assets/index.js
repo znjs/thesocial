8e24c53b16778da21e6a6f41a1692c6b36905824
znjs <jasrikar@gmail.com>
2022-05-16 01:35:57 +0530

    refactor: minor

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as logo_title } from "./logi-title.png";
+export { default as logo } from "./logo.png";
+export { default as title } from "./title.png";
+export { default as authImg } from "./auth.jpg";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import { makeServer } from "./server";
 
 // Call make Server
@@ -11,5 +11,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
