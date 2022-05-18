8a2cc941a653adaff8c59a95ed3885e154ac2085
znjs <jasrikar@gmail.com>
2022-05-18 18:34:08 +0530

    refactor: minor changes

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { logo } from "../../assets";
 
 function SignUp() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({ firstName: "", lastName: "", email: "", password: "" });
   return (
     <div className="flex h-screen w-screen bg-slate-800 text-center">
       <div className="flex justify-center items-center flex-grow min-w-fit">
@@ -16,10 +19,26 @@ function SignUp() {
 
             <h2 className=" text-2xl my-3">Sign Up</h2>
             <div className="flex justify-between items-center">
-              <label className="text-xl w-40" htmlFor="fullName">
-                FullName:
+              <label className="text-xl w-40" htmlFor="firstName">
+                FirstName:
               </label>
               <input
+                value={user.firstName}
+                onChange={(e) => setUser((prev) => ({ ...prev, firstName: e.target.value }))}
+                type="text"
+                name="fullName"
+                placeholder="John Doe"
+                autoComplete="off"
+                className=" text-center bg-black py-3 my-4 border hover: border-gray-500 rounded-xl shadow text-base font-sans"
+              />
+            </div>
+            <div className="flex justify-between items-center">
+              <label className="text-xl w-40" htmlFor="fullName">
+                LastName:
+              </label>
+              <input
+                value={user.lastName}
+                onChange={(e) => setUser((prev) => ({ ...prev, lastName: e.target.value }))}
                 type="text"
                 name="fullName"
                 placeholder="John Doe"
@@ -32,6 +51,8 @@ function SignUp() {
                 Email:
               </label>
               <input
+                value={user.email}
+                onChange={(e) => setUser((prev) => ({ ...prev, email: e.target.value }))}
                 type="email"
                 name="email"
                 placeholder="john.doe@gmail.com"
@@ -44,6 +65,8 @@ function SignUp() {
                 Password:
               </label>
               <input
+                value={user.password}
+                onChange={(e) => setUser((prev) => ({ ...prev, password: e.target.value }))}
                 type="password"
                 name="password"
                 placeholder="password . . ."
@@ -52,10 +75,20 @@ function SignUp() {
             </div>
             <button
               type="button"
-              className="text-center w-full py-4 bg-sky-500 hover:bg-sky-400 rounded-md cursor-not-allowed cursor-pointer">
+              onClick={() => {
+                if (!!user.email && !!user.password && user.lastName && user.firstName)
+                  navigate("/");
+              }}
+              className={`text-center w-full py-4 bg-sky-500 hover:bg-sky-400 rounded-md ${
+                !!user.email && !!user.password && !!user.firstName && !!user.lastName
+                  ? "cursor-pointer"
+                  : "cursor-not-allowed"
+              }  `}>
               SIGN UP
             </button>
-            <p className="text-sm font-sans font-medium text-gray-400 underline select-none cursor-pointer">
+            <p
+              onClick={() => navigate("/login")}
+              className="text-sm font-sans font-medium text-gray-400 underline select-none cursor-pointer">
               Already have an Account? Sign up
             </p>
           </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,64 +1,13 @@
 import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
-import { logo } from "../../assets";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { Navbar, Suggestion } from "../../components";
 import "./home.css";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <div className="bg-[#11161b] text-gray-50 h-screen w-screen flex flex-col">
-      <nav className="navbar bg-base-100 px-4 py-2">
-        <div className="flex-none">
-          <img src={logo} alt="logo" className="h-10" />
-        </div>
-        <div className="flex-1">
-          <button className="ff-nickainley text-2xl mx-4">The Social</button>
-        </div>
-        <div className="dropdown dropdown-end">
-          <label tabIndex="0" className="btn m-1 border-0">
-            <div className="indicator text-2xl">
-              <span className="indicator-item badge rounded-full ">4</span>
-              <div className="grid w-10 h-10 bg-base-300 place-items-center">
-                <i className="fa-solid fa-bell"></i>
-              </div>
-            </div>
-          </label>
-          <ul
-            tabIndex="0"
-            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li className="bg-[#454d50] hover:bg-[#2c3133]">
-              <p>Notification 1</p>
-            </li>
-            <li className="bg-[#454d50] hover:bg-[#2c3133]">
-              <p>Notification 2</p>
-            </li>
-          </ul>
-        </div>
-        <div className="dropdown dropdown-end  ">
-          <label tabIndex="0" className="btn m-1 bg-[#424F55] outline-none border-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-            </svg>
-          </label>
-          <ul
-            tabIndex="0"
-            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li className="bg-[#424F55] hover:bg-[#2c3133]">
-              <p>Profile</p>
-            </li>
-            <li className="bg-red-500 hover:bg-red-600">
-              <p>Logout</p>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar />
       <div className="w-screen max-w-[1600px] homeContainer flex mx-auto mt-2">
         <div className="w-1/5 p-2">
           <div className="flex flex-col items-center background-clr py-3 rounded-lg">
@@ -66,10 +15,17 @@ function Home() {
               <img
                 src="https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1101&q=80"
                 alt="Profile pic"
-                className="rounded-full h-28 w-28 object-cover"
+                className="rounded-full h-28 w-28 object-cover cursor-pointer"
+                onClick={() => navigate("/profile")}
               />
-              <p className="font-semibold text-lg">Alex Stroganoff</p>
-              <p className="text-lg italic">@Alex</p>
+              <p
+                onClick={() => navigate("/profile")}
+                className="font-semibold text-lg cursor-pointer">
+                Alex Stroganoff
+              </p>
+              <p onClick={() => navigate("/profile")} className="text-lg italic cursor-pointer">
+                @Alex
+              </p>
             </div>
             <div className="flex justify-evenly w-full">
               <div className="text-center flex-grow border-r border-slate-50">
@@ -147,7 +103,9 @@ function Home() {
         <div className="main-container flex-grow p-2">
           <Outlet />
         </div>
-        <div className=" w-1/5 p-2"></div>
+        <div className=" w-1/5 p-2">
+          <Suggestion />
+        </div>
       </div>
     </div>
   );
