a8c0918c28a1d8a8295474f8cdd32cde344642f3
znjs <jasrikar@gmail.com>
2022-06-09 02:12:55 +0530

    fix: circular error and backend

--- src/backend/controllers/PostController.js
+++ src/backend/controllers/PostController.js
@@ -168,7 +168,7 @@ export const likePostHandler = function (schema, request) {
     }
     post.likes.dislikedBy = post.likes.dislikedBy.filter((currUser) => currUser._id !== user._id);
     post.likes.likeCount += 1;
-    post.likes.likedBy.push(user);
+    post.likes.likedBy.push(JSON.parse(JSON.stringify(user)));
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
   } catch (error) {
@@ -213,7 +213,7 @@ export const dislikePostHandler = function (schema, request) {
     }
     post.likes.likeCount -= 1;
     const updatedLikedBy = post.likes.likedBy.filter((currUser) => currUser._id !== user._id);
-    post.likes.dislikedBy.push(user);
+    post.likes.dislikedBy.push(JSON.parse(JSON.stringify(user)));
     post = { ...post, likes: { ...post.likes, likedBy: updatedLikedBy } };
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -9,20 +9,22 @@ import { formatDate } from "../utils/authUtils";
 export const posts = [
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Had a wonderful day in bangalore!!",
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "adarshbalika",
+    username: "znjs@znjs.com",
+    profileImage: "https://pbs.twimg.com/profile_images/1414657404843663368/rXyiNk3G_400x400.jpg",
     createdAt: formatDate(),
     updatedAt: formatDate(),
     comments: [
       {
         _id: uuid(),
-        username: "shubhamsoni",
+        username: "Laurel@gmail.com",
+        profileImage:
+          "https://images.unsplash.com/photo-1584270413639-d5ee397272cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         text: "Interesting",
         votes: {
           upvotedBy: [],
@@ -31,7 +33,9 @@ export const posts = [
       },
       {
         _id: uuid(),
-        username: "sohamshah",
+        username: "Alex@gmail.com",
+        profileImage:
+          "https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1101&q=80",
         text: "Wow!",
         votes: {
           upvotedBy: [],
@@ -42,19 +46,22 @@ export const posts = [
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Random post on a random day",
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "shubhamsoni",
-
+    username: "Laurel@gmail.com",
+    profileImage:
+      "https://images.unsplash.com/photo-1584270413639-d5ee397272cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     comments: [
       {
         _id: uuid(),
-        username: "shubhamsoni",
+        username: "Alex@gmail.com",
+        profileImage:
+          "https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1101&q=80",
+
         text: "Interesting",
         votes: {
           upvotedBy: [],
@@ -63,7 +70,9 @@ export const posts = [
       },
       {
         _id: uuid(),
-        username: "sohamshah",
+        username: "Laurel@gmail.com",
+        profileImage:
+          "https://images.unsplash.com/photo-1584270413639-d5ee397272cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         text: "Wow!",
         votes: {
           upvotedBy: [],
@@ -71,6 +80,88 @@ export const posts = [
         },
       },
     ],
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "znjs@znjs.com",
+    profileImage: "https://pbs.twimg.com/profile_images/1414657404843663368/rXyiNk3G_400x400.jpg",
+    comments: [
+      {
+        _id: uuid(),
+        username: "Alex@gmail.com",
+        profileImage:
+          "https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1101&q=80",
+        text: "Interesting",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+      {
+        _id: uuid(),
+        username: "Laurel@gmail.com",
+        profileImage:
+          "https://images.unsplash.com/photo-1584270413639-d5ee397272cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        text: "Wow!",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+    ],
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: "Javascript rocks",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "Laurel@gmail.com",
+    profileImage:
+      "https://images.unsplash.com/photo-1584270413639-d5ee397272cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    comments: [],
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: "Javascript rocks",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "Alex@gmail.com",
+    profileImage:
+      "https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1101&q=80",
+    comments: [],
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: "Javascript!! It's gonna be legen—wait for it—dary",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "neil@gmail.com",
+    profileImage: "https://media1.fdncms.com/saltlake/imager/u/zoom/14330770/barney_stinson.jpg",
+    comments: [],
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
