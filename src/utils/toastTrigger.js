fb2f9f23e5351b67fb2f6e74170ac135e56d7a10
znjs <jasrikar@gmail.com>
2022-06-17 00:05:58 +0530

    feat: added toast component

---
+++ src/utils/toastTrigger.js
@@ -0,0 +1,39 @@
+import { toast } from "react-toastify";
+
+export const triggerToast = (type, message) => {
+  switch (type) {
+    case "success":
+      toast.success(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      break;
+    case "error":
+      toast.error(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      break;
+    default:
+      toast(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      break;
+  }
+};
